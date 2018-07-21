da1c0f5ee5b69589ed5f570461988585d2b27184
maricajr <maricaneston38@gmail.com>
2018-07-21 13:37:13 -0700

    alt finish()

--- app/src/main/java/com/example/maricajr/androidlock/MainActivity.java
+++ app/src/main/java/com/example/maricajr/androidlock/MainActivity.java
@@ -1,13 +1,84 @@
 package com.example.maricajr.androidlock;
 
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    public static final int RESULT_ENABLE = 11;
+    private DevicePolicyManager devicePolicyManager;
+    private ActivityManager activityManager;
+    private ComponentName compName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        devicePolicyManager = (DevicePolicyManager) getSystemService(DEVICE_POLICY_SERVICE);
+        activityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        compName = new ComponentName(getApplicationContext(), Administrator.class);
+
+        boolean active = devicePolicyManager.isAdminActive(compName);
+
+        if (active){
+
+            Intent data=getIntent();
+            String value=data.getStringExtra("unlock");
+            if (value != null){
+                finish();
+            }else {
+                lockDevice();
+            }
+
+
+        }else {
+            enableLockAdminPermission();
+        }
+
     }
+
+    private void enableLockAdminPermission() {
+
+        Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+        intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, compName);
+        intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION,
+                "Additional text explaining why we need this permission");
+        startActivityForResult(intent, RESULT_ENABLE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        switch(requestCode) {
+
+            case RESULT_ENABLE :
+                if (resultCode == Activity.RESULT_OK) {
+
+                    lockDevice();
+
+                } else {
+                    super.finish();
+                    Toast.makeText(MainActivity.this,
+                            "Access Denied", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void lockDevice() {
+        devicePolicyManager.lockNow();
+    }
+
 }
---
+++ app/src/main/java/com/example/maricajr/androidlock/PhoneLockReceiver.java
@@ -0,0 +1,42 @@
+package com.example.maricajr.androidlock;
+
+import android.app.KeyguardManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+public class PhoneLockReceiver extends BroadcastReceiver {
+
+    KeyguardManager keyguardManager;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        keyguardManager= (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
+        assert keyguardManager != null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+            if (keyguardManager.isDeviceLocked()){
+                //locked
+
+            }else {
+                //unlocked
+                Intent intent1=new Intent(context,MainActivity.class);
+                intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent1.putExtra("unlock","1");
+                context.startActivity(intent1);
+            }
+        }else {
+            //lower API
+            if (keyguardManager.inKeyguardRestrictedInputMode()){
+                //locked
+
+            }else {
+                //unlocked
+                Intent intent1=new Intent(context,MainActivity.class);
+                intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent1.putExtra("unlock","1");
+                context.startActivity(intent1);
+            }
+        }
+    }
+}
